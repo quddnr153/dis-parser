f1500d988b9abf54ea5cfde795f06650e43f1bf8
Byungwook Lee <quddnr153@gmail.com>
2017-10-07 23:38:59 +0900

    mic test

--- src/main/java/io/dis/parser/receiver/controller/ReceiverController.java
+++ src/main/java/io/dis/parser/receiver/controller/ReceiverController.java
@@ -3,6 +3,10 @@ package io.dis.parser.receiver.controller;
 import io.dis.parser.receiver.model.Sentence;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReceiverController {
     @GetMapping("/receiver")
-    public Sentence receiveSentence(@RequestParam(value = "sentence") String clientSentence) {
+    public Sentence receiveSentence(@RequestParam(value = "sentence") String clientSentence) throws IOException, InterruptedException {
         if (clientSentence.isEmpty()) {
             throw new IllegalArgumentException("Sentence sent by client should not be empty.");
         }
 
         log.info("Get receiveSentence. sentence = {}", clientSentence);
+
+        if (clientSentence.toLowerCase().equals("jarvis")) {
+            Process process = Runtime.getRuntime().exec("python ~/Document/git-projects/dis-tts/hello-jarvis.py");
+            process.waitFor();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine())!= null) {
+                log.info(line);
+            }
+        }
 
         Sentence sentence = new Sentence();
         sentence.setId(1L);
